db688f3e53aef25c5b7fbca006eaa38e0437a20b
Drakiel <tristangautier2003@gmail.com>
2025-04-24 11:22:42 +0200

    Refactor LodgerButton component to include className prop and improve button styling logic

--- app/[locale]/deposit/page.tsx
+++ app/[locale]/deposit/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Button } from "@heroui/react";
@@ -6,99 +6,107 @@ import FormEntry from "@/app/ui/components/FormEntry";
 import LodgerButton from "@/app/ui/components/LodgerButton";
 
 export default function Page() {
-    const [loading, setLoading] = useState(true);
-    const [formData, setFormData] = useState({
-        count: 0,
-        number: 0,
-        dropdown: new Set<string>(),
-        yesno: null,
-    });
+  const [loading, setLoading] = useState(true);
+  const [formData, setFormData] = useState({
+    count: 0,
+    number: 0,
+    dropdown: new Set<string>(),
+    yesno: null,
+  });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // ici on va recupérer les données dont on a besoin
-                await new Promise(resolve => setTimeout(resolve, 1500));
-                setLoading(false);
-            } catch (error) {
-                console.error("Erreur lors du chargement:", error);
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, []);
-
-    const handleUpdate = (key: string, value: any) => {
-        setFormData((prev) => ({ ...prev, [key]: value }));
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // ici on va recupérer les données dont on a besoin
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+        setLoading(false);
+      } catch (error) {
+        console.error("Erreur lors du chargement:", error);
+        setLoading(false);
+      }
     };
+    fetchData();
+  }, []);
 
-    const handleSubmit = () => {
-        console.log("Form Data:", formData);
-        // Vous pouvez envoyer les données au serveur ici
-        // fais un mock de l'envoi de données en affichant les données dans la console
-        alert("Données envoyées avec succès !");
-        console.log("Données envoyées:", formData);
-        
-    };
+  const handleUpdate = (key: string, value: any) => {
+    setFormData((prev) => ({ ...prev, [key]: value }));
+  };
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            {loading ? (
-                // Logo scintillant (loader)
-                <div className={"flex flex-col loader-animation text-center"}>
-                    <Image
-                        src="/logo_lodger.png"
-                        width={147}
-                        height={32}
-                        className=""
-                        alt="lodger logo"
-                    />
-                    <p>Loading ... </p>
-                </div>
-            ) : (
-                // Contenu final après chargement
-                <div className="text-center w-full flex flex-col">
-                    <h1 className="text-2xl font-bold">Bienvenue sur Lodger !</h1>
-                    <p className="text-gray-600">Le contenu est maintenant affiché.</p>
-                    <FormEntry
-                        title="Nombre de chambres"
-                        description="Renseigner le nombre de chambres :"
-                        logo="/icons/bed-icon.svg"
-                        type="count"
-                        onUpdate={(value) => handleUpdate("count", value)}
-                    />
-                    <FormEntry
-                        title="Superficie"
-                        description="Renseigner la superficie de votre bien :"
-                        logo="/icons/superficie-icon.svg"
-                        type="number"
-                        onUpdate={(value) => handleUpdate("number", value)}
-                    />
-                    <FormEntry
-                        title="Espaces Partagés"
-                        description="Sélectionner les espaces partagés :"
-                        logo="/icons/matter-icon.svg"
-                        type="dropdown"
-                        onUpdate={(value) => handleUpdate("dropdown", value)}
-                    />
-                    <FormEntry
-                        title="Meublé"
-                        description="Le bien est-il meublé ?"
-                        logo="/icons/sofa-icon.svg"
-                        type="yesno"
-                        onUpdate={(value) => handleUpdate("yesno", value)}
-                    />
-                    <Button onPress={handleSubmit} className="mt-4">
-                        Soumettre
-                    </Button>
-                    <div id="button-mock" className="flex flex-row justify-center space-x-3 mt-4">
-                        <LodgerButton type="default" label="Enregistrer et Quitter" />
-                        <LodgerButton type="full-success" label="Suivant" />
-                        <LodgerButton type="no-border" label="Retour" />
-                    </div>
-                </div>
-            )}
+  const handleSubmit = () => {
+    console.log("Form Data:", formData);
+    // Vous pouvez envoyer les données au serveur ici
+    // fais un mock de l'envoi de données en affichant les données dans la console
+    alert("Données envoyées avec succès !");
+    console.log("Données envoyées:", formData);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      {loading ? (
+        // Logo scintillant (loader)
+        <div className={"flex flex-col loader-animation text-center"}>
+          <Image
+            src="/logo_lodger.png"
+            width={147}
+            height={32}
+            className=""
+            alt="lodger logo"
+          />
+          <p>Loading ... </p>
         </div>
-    );
+      ) : (
+        // Contenu final après chargement
+        <>
+          <div className="text-center w-full flex flex-col">
+            <h1 className="text-2xl font-bold">Bienvenue sur Lodger !</h1>
+            <p className="text-gray-600">Le contenu est maintenant affiché.</p>
+            <FormEntry
+              title="Nombre de chambres"
+              description="Renseigner le nombre de chambres :"
+              logo="/icons/bed-icon.svg"
+              type="count"
+              onUpdate={(value) => handleUpdate("count", value)}
+            />
+            <FormEntry
+              title="Superficie"
+              description="Renseigner la superficie de votre bien :"
+              logo="/icons/superficie-icon.svg"
+              type="number"
+              onUpdate={(value) => handleUpdate("number", value)}
+            />
+            <FormEntry
+              title="Espaces Partagés"
+              description="Sélectionner les espaces partagés :"
+              logo="/icons/matter-icon.svg"
+              type="dropdown"
+              onUpdate={(value) => handleUpdate("dropdown", value)}
+            />
+            <FormEntry
+              title="Meublé"
+              description="Le bien est-il meublé ?"
+              logo="/icons/sofa-icon.svg"
+              type="yesno"
+              onUpdate={(value) => handleUpdate("yesno", value)}
+            />
+          </div>
+          <div className="flex flex-row justify-center space-x-3 mt-4 self-end">
+          <LodgerButton
+              onPress={handleSubmit}
+              className="mt-4"
+              label="Soumettre"
+              type="full-success"
+            />
+          </div>
+          <div
+              id="button-mock"
+              className="flex flex-row justify-center space-x-3 mt-4"
+            >
+              <LodgerButton type="default" label="Enregistrer et Quitter" />
+              <LodgerButton type="full-success" label="Suivant" />
+              <LodgerButton type="no-border" label="Retour" />
+            </div>
+        </>
+      )}
+    </div>
+  );
 }
-
--- app/ui/components/LodgerButton.tsx
+++ app/ui/components/LodgerButton.tsx
@@ -4,20 +4,22 @@ export default function LodgerButton({
   type,
   label,
   onPress,
+  className,
 }: {
   type?: "default" | "full-success" | "no-border";
   onPress?: () => void;
   label: string;
+  className?: string;
 }) {
   return (
     <>
-      {type === "default" ? (
-        <Button onPress={onPress} variant="shadow" className="border-2 border-primary-100 text-primary-100 bg-white font-semibold">{label}</Button>
-      ) : type === "full-success" ? (
-        <Button onPress={onPress} color="success" variant="shadow" className="text-white font-semibold">{label}</Button>
+      {type === "full-success" ? (
+        <Button onPress={onPress} color="success" variant="shadow" className={"text-white font-semibold" + className}>{label}</Button>
       ) : type === "no-border" ? (
-        <Button onPress={onPress} variant="shadow" className="border-1 border-gray-200 font-semibold text-primary-100 bg-white">{label}</Button>
-      ) : null}
+        <Button onPress={onPress} variant="shadow" className={"border-1 border-gray-200 font-semibold text-primary-100 bg-white" + className}>{label}</Button>
+      ) : (
+        <Button onPress={onPress} variant="shadow" className={"border-2 border-primary-100 text-primary-100 bg-white font-semibold " + className}>{label}</Button>
+      )}
     </>
   );
 }
