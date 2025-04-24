8130ff1296c5c2d3b94d9e0635ed548d8967c55e
Cyprien GAUDIN <cyprien.gaudin@supdevinci-edu.fr>
2025-04-24 16:02:23 +0200

    add infos for Logement

--- app/api/deposit/deposit.ts
+++ app/api/deposit/deposit.ts
@@ -20,14 +20,31 @@ class Deposit {
                 return { status: 'ERROR', message: "Aucun compte trouvé avec cet email." };
             }
 
+
             await prisma.logement.create({
                 data: {
                     draft: data.draft,
+                    displayPreciseAddress: data.displayPreciseAddress,
+                    postalCode: data.postalCode,
+                    city: data.city,
+                    street: data.street,
+                    numero: data.numero,
+                    addressComplement: data.addressComplement,
+                    stopProcess: data.stopProcess,
+                    superficie: data.superficie,
+                    roomNumber: data.roomNumber,
+                    bedroomNumber: data.bedroomNumber,
+                    furnished: data.furnished,
+                    bathRoomSpace: data.bathroomSpace,
+                    powderRoomSpace: data.powderRoomSpace,
+                    appartmentFloor: data.appartmentFloor,
+                    kWhEP: data.kWhEP,
+                    kgCO2: data.kgCO2,
                     accountId: account.id,
                 }
             });
 
-            return { status: 'SUCCESS', message: 'Inscription réussie' };
+            return { status: 'SUCCESS', message: 'Save Informations' };
         } catch (error) {
             console.error("Erreur lors de la sauvegarde des données :", error);
             return { status: 'ERROR', message: 'Une erreur est survenue lors de la sauvegarde.' };
--- app/api/interface.ts
+++ app/api/interface.ts
@@ -1,4 +1,24 @@
 export interface LogementData {
     email: string;
-    draft: string;
+    typeOfLogement: string;
+    typeOfProperty: string;
+    draft: boolean;
+    stopProcess : string;
+    logementType: string;
+    propertyType: string;
+    displayPreciseAddress: boolean;
+    postalCode: string;
+    city: string;
+    street: string;
+    numero: string;
+    addressComplement:string;
+    superficie: string;
+    roomNumber: string;
+    bedroomNumber: string;
+    furnished: boolean;
+    bathRoomSpace: bigint;
+    powderRoomSpace: bigint;
+    appartmentFloor: bigint;
+    kWhEP: bigint;
+    kgCO2: bigint;
 }
