5df2ee75c0a7421458c38ad25bcfde2395c459d3
SupRemaZie <Damien.Porchet.Etu@univ-lemans.fr>
2025-04-24 18:24:41 +0200

    fix : space between

--- app/[locale]/deposit/layout.tsx
+++ app/[locale]/deposit/layout.tsx
@@ -2,11 +2,11 @@ import SideNav from '@/app/ui/components/sidenav';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-dashboard">
+    <div className="flex max-h-screen overflow-hidden flex-col md:flex-row md:overflow-hidden bg-dashboard">
       <div className="w-full flex-none md:w-[297px] bg-white border-r-2 border-y-dashboard-separator" >
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow h-screen overflow-hidden md:overflow-hidden ">{children}</div>
     </div>
   );
 }
--- app/[locale]/deposit/page.tsx
+++ app/[locale]/deposit/page.tsx
@@ -22,54 +22,61 @@ export default function Page() {
     };
 
     const handleSubmit = () => {
+
         console.log("Données envoyées:", formData);
     };
 
     return (
-        <div className="font-[500] flex flex-col  min-h-screen py-2 scrollbar-hide">
-            <div id="header" className="w-full flex flex-row justify-between  items-center">
-                <div className="flex flex-col">
-                   <span id="page-title" className="text-[#02504D] font-" >
+        <div className="max-h-screen w-full">
+            <div className="font-[500] flex flex-col  min-h-screen py-2 scrollbar-hide">
+                <div id="header" className="w-full flex flex-row justify-between  items-center">
+                    <div className="flex flex-col">
+                   <span id="page-title" className="text-[#02504D] font-">
                      {trans('header.title')}
                    </span>
-                   <span className="font-bold text-xl">
+                        <span className="font-bold text-xl">
                      {trans('header.question')}
                    </span>
+                    </div>
+                    <LodgerButton type="default" label={trans("actions.saveAndQuit")}></LodgerButton>
                 </div>
-                <LodgerButton type="default" label={trans("actions.saveAndQuit")} ></LodgerButton>
-            </div>
-            <section id="content" className="pt-8 text-[#02504D]" >
+                <section id="content" className="pt-8 text-[#02504D]">
                 <span className="font-[700] text-sm">
                     {trans('content.title')}
                 </span>
-                <div className="flex flex-row gap-4 mt-4 font-[600]">
-                    <Button  onPress={() => handleUpdate("choosed", "appartement")}
-                        variant="bordered" color="primary"className={`flex flex-col w-1/4 h-1/4 bg-white text-[#02504D] ${
-                        formData.choosed === "appartement" ? "border-2 border-[#02DB82]" : ""
-                    }`}>
-                        <Image src="/appartment_choice_picture.png" alt="appartment choice" width={174} height={174} className="w-2/3"/>
-                        <span className="text-[#02504D] font-bold text-base mb-6 ">
+                    <div className="flex flex-row gap-4 mt-4 font-[600]">
+                        <Button onPress={() => handleUpdate("choosed", "appartement")}
+                                variant="bordered" color="primary"
+                                className={`flex flex-col w-1/4 h-1/4 bg-white text-[#02504D] ${
+                                    formData.choosed === "appartement" ? "border-2 border-[#02DB82]" : ""
+                                }`}>
+                            <Image src="/appartment_choice_picture.png" alt="appartment choice" width={174} height={174}
+                                   className="w-2/3"/>
+                            <span className="text-[#02504D] font-bold text-base mb-6 ">
                             Appartement
                         </span>
-                    </Button>
-                    <Button onPress={() => handleUpdate("choosed", "house")} variant="bordered" color="primary"
-                            className={`flex flex-col w-1/4 h-1/4 bg-white text-[#02504D] ${formData.choosed === "house" ? "border-2 border-[#02DB82]" : ""}`}>
+                        </Button>
+                        <Button onPress={() => handleUpdate("choosed", "house")} variant="bordered" color="primary"
+                                className={`flex flex-col w-1/4 h-1/4 bg-white text-[#02504D] ${formData.choosed === "house" ? "border-2 border-[#02DB82]" : ""}`}>
 
-                        <Image src="/house_choice_picture.png" alt="house choice" width={174} height={174} className="w-2/3"/>
-                        <span className="text-[#02504D] font-bold text-base mb-6">
+                            <Image src="/house_choice_picture.png" alt="house choice" width={174} height={174}
+                                   className="w-2/3"/>
+                            <span className="text-[#02504D] font-bold text-base mb-6">
                             Maison
                         </span>
-                    </Button>
-                    <footer className="flex flex-row mt-80">
-                        <LodgerButton onPress={()=>handleSubmit()} isDisabled={!formData.choosed} label="retour" type={"default"}></LodgerButton>
-                        <LodgerButton onPress={()=>handleSubmit()} isDisabled={!formData.choosed} label="suivant" type={"full-success"}></LodgerButton>
-                    </footer>
-
-
-
-
-                </div>
-            </section>
+                        </Button>
+                    </div>
+                </section>
+            </div>
+            <footer
+                className="flex flex-row justify-between  w-full bg-white ">
+                <LodgerButton onPress={() => handleSubmit()} isDisabled={!formData.choosed} label="retour"
+                              type={"default"}></LodgerButton>
+                <LodgerButton onPress={() => handleSubmit()} isDisabled={!formData.choosed} label="suivant"
+                              type={"full-success"}></LodgerButton>
+            </footer>
         </div>
+
+
     );
 }
