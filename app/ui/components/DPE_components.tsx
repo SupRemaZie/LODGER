9655c29ba5d12032f26d24045e290f674060ecf5
Dr4kiel <tristangautier2003@gmail.com>
2025-04-25 14:33:20 +0200

    feat: add RateRadio and RateComponent for enhanced radio button functionality

---
+++ app/ui/components/DPE_components.tsx
@@ -0,0 +1,51 @@
+import { RadioGroup, useRadio, VisuallyHidden, cn } from "@heroui/react";
+import { RadioProps } from "@heroui/react";
+
+export const RateRadio = (props: RadioProps) => {
+  const {
+    Component,
+    children,
+    description,
+    getBaseProps,
+    getWrapperProps,
+    getInputProps,
+    getLabelProps,
+    getLabelWrapperProps,
+    getControlProps,
+  } = useRadio(props);
+
+  return (
+    <Component
+      {...getBaseProps()}
+      className={cn(
+        "group inline-flex items-center hover:opacity-70 active:opacity-50 justify-between flex-row-reverse tap-highlight-transparent",
+        "max-w-[300px] cursor-pointer border-2 border-default rounded-lg gap-4 p-4",
+        "data-[selected=true]:border-primary"
+      )}
+    >
+      <VisuallyHidden>
+        <input {...getInputProps()} />
+      </VisuallyHidden>
+      <span {...getWrapperProps()}>
+        <span {...getControlProps()} />
+      </span>
+      <div {...getLabelWrapperProps()}>
+        {children && <span {...getLabelProps()}>{children}</span>}
+        {description && (
+          <span className="text-small text-foreground opacity-70">
+            {description}
+          </span>
+        )}
+      </div>
+    </Component>
+  );
+};
+
+export const RateComponent = (props: any) => {
+  const { children, ...rest } = props;
+  return (
+    <RadioGroup {...rest} className="flex flex-col gap-4">
+      {children}
+    </RadioGroup>
+  );
+};
