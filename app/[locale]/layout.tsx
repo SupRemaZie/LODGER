4b987f38731971accfa467b06b857d84a160c377
SupRemaZie <Damien.Porchet.Etu@univ-lemans.fr>
2025-04-28 17:52:32 +0200

    fix : npm build error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,38 +1,42 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import {Providers} from "../context/providers";
+import { Providers } from "../context/providers";
 import { BreadcrumbProvider } from '../context/BreadcrumbContext';
-import {NextIntlClientProvider, hasLocale} from 'next-intl';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
-import {FormDataProvider} from "@/app/context/FormDataContext";
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import { FormDataProvider } from "@/app/context/FormDataContext";
 
- 
+import { getMessages } from 'next-intl/server'; // <== important
+
 export default async function RootLayout({
-  children,
-  params
-}: {
+                                           children,
+                                           params
+                                         }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 }) {
-  const {locale} = params;
+  const { locale } = params;
+
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+
+  const messages = await getMessages(); // <== ici tu charges les messages
+
   return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider>
-          <Providers>
-            <BreadcrumbProvider>
-              <FormDataProvider>
-                {children}
-              </FormDataProvider>
-            </BreadcrumbProvider>
-          </Providers>
-        </NextIntlClientProvider>
+      <html lang={locale}>
+      <body className={inter.className}>
+      <NextIntlClientProvider locale={locale} messages={messages}>
+        <Providers>
+          <BreadcrumbProvider>
+            <FormDataProvider>
+              {children}
+            </FormDataProvider>
+          </BreadcrumbProvider>
+        </Providers>
+      </NextIntlClientProvider>
       </body>
-    </html>
-   
+      </html>
   );
 }
