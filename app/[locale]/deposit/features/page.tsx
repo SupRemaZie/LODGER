71e9aa889fc9320687b39cef21a8994312b0f3d4
Cyprien GAUDIN <cyprien.gaudin@supdevinci-edu.fr>
2025-04-29 15:11:59 +0200

    add text in page

--- app/[locale]/deposit/features/page.tsx
+++ app/[locale]/deposit/features/page.tsx
@@ -28,7 +28,8 @@ export default function Page() {
         <div className="w-full">
             <div className="font-[500] flex flex-col  min-h-screen p-12 scrollbar-hide">
                 <Header title={trans("stepTwo.stepTwo-subOne.title")} question={trans("stepTwo.stepTwo-subOne.question")} />
-                <section id="content" className="flex flex-col pt-8 text-[#02504D] overflow-y-auto">
+                <section id="content" className="flex justify-center items-center text-[#02504D] min-h-screen">
+                    <h1 className="text-4xl font-bold">{trans("stepTwo.stepTwo-subOne.text")}</h1>
                 </section>
             </div>
             <Footer onPrevious={handlePrevious} onNext={handleNext} requiredField={formData.typeOfProperty} step={2}/>
