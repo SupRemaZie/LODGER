9a6f96fa869bf6c2411be302f578a67a5c9629b4
Cyprien GAUDIN <cyprien.gaudin@supdevinci-edu.fr>
2025-04-29 11:52:25 +0200

    add next to page dpe

--- app/[locale]/deposit/information/page.tsx
+++ app/[locale]/deposit/information/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
     const pathName = usePathname()
 
     const handleNext =() =>{
-        router.push(`${pathName}/DPE`)
+        router.push(`dpe`)
         console.log(formData)
     }
     const handlePrevious=()=>{
