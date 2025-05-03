f87b51ff20e7ed3a68c9fb9dc7259668d5990495
tgrignon <thibaud.grignon@etudiant.univ-lr.fr>
2025-05-04 00:44:13 +0200

    add Text component

--- app/ui/components/Text.tsx
+++ app/ui/components/Text.tsx
@@ -1,7 +1,39 @@
 "use client";
 
-const Text = () => {
-    return <div></div>
+import React, {useState} from "react";
+
+const Text = ({id, title, description, placeholder}: {
+    id: string,
+    title: string,
+    description: string,
+    placeholder: string
+}) => {
+
+    const [query, setQuery] = useState('');
+
+    const handleInput = async (e: any) => {
+        const value = e.target.value;
+        setQuery(value);
+    }
+
+    return (
+        <div className="w-3/5 mb-4 mt-4">
+            <div className="mb-4">
+                <div className="text-primary-100 font-bold">
+                    {title}
+                </div>
+                <div className="text-gray-600">{description}</div>
+            </div>
+            <input
+                id={id}
+                value={query}
+                onChange={handleInput}
+                placeholder={placeholder}
+                className="p-2 shadow rounded-xl w-full"
+                style={{ border: '1px solid lightgray'}}
+            />
+        </div>
+    );
 }
 
 export default Text;
