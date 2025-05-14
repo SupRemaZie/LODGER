90b0b3061a837ab7f5191b5daccf58d9c233b7bd
tgrignon <thibaud.grignon@etudiant.univ-lr.fr>
2025-05-15 00:38:25 +0200

    add input field to page + logic

--- app/[locale]/deposit/localisation/page.tsx
+++ app/[locale]/deposit/localisation/page.tsx
@@ -6,6 +6,7 @@ import Footer from "@/app/ui/components/Footer";
 import {usePathname, useRouter} from "next/navigation";
 import LodgerButton from "@/app/ui/components/LodgerButton";
 import AddressMap from "@/app/ui/components/AddressMap";
+import Text from "@/app/ui/components/Text";
 
 export default function Page() {
     const trans = useTranslations('PropertydepositPage')
@@ -29,14 +30,42 @@ export default function Page() {
         <div className="flex flex-col min-h-screen w-full">
             <Header title={trans("stepOne.stepOne-subTwo.title")} question={trans("stepOne.stepOne-subTwo.question")} />
             <main className="flex-1 px-16 pt-8 pb-8 overflow-y-auto text-[#02504D]">
-                <section id="content" className="text-[#02504D]">
-                    <span className="font-[700] text-sm">
-                        {trans('stepOne.stepOne-subTwo.subtitle')}
-                    </span>
-                    <AddressMap title="" description="" targetInputIds={{city: "", postcode: "", street: ""}}/>
+                <section id="content" className="text-[#02504D] flex flex-col">
+                    <AddressMap title={trans('stepOne.stepOne-subTwo.subtitle')} description="" targetInputIds={{city: "cityField", postcode: "postcodeField", street: "streetField"}}/>
+                    <Text
+                        id="cityField"
+                        title="Ville"
+                        description=""
+                        placeholder="Paris"
+                        value={formData.city}
+                        onChange={(value) => handleUpdate('city', value)}/>
+                    <Text
+                        id="postcodeField"
+                        title="Code Postal"
+                        description=""
+                        placeholder="75009"
+                        value={formData.postalCode}
+                        onChange={(value) => handleUpdate('postalCode', value)}/>
+                    <Text
+                        id="streetField"
+                        title="Numéro et nom de voie"
+                        description=""
+                        placeholder="1 Avenue des Champs-Elysée"
+                        value={formData.street}
+                        onChange={(value) => handleUpdate('street', value)}/>
+                    <Text
+                        id="detailsField"
+                        title="Bâtiment, appartement, résidence, étage"
+                        description=""
+                        placeholder="1er étage"/>
                 </section>
             </main>
-            <Footer onPrevious={handlePrevious} onNext={handleNext} requiredField={formData.typeOfProperty} step={1}/>
+            <Footer
+                onPrevious={handlePrevious}
+                onNext={handleNext}
+                requiredField={formData.city}
+                step={1}
+            />
 
         </div>
     )
